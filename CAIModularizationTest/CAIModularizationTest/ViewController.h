438140f310bd8b0a0c22f8ea89c2ed3623e552e2
caiyanpeng <1104990855@qq.com>
2019-02-20 10:57:21 +0800

    init project

---
+++ CAIModularizationTest/CAIModularizationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CAIModularizationTest
+//
+//  Created by Chengguangfa on 2019/2/20.
+//  Copyright © 2019年 com.medosport.mo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CAIModularizationTest/CAIModularizationTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CAIModularizationTest
+//
+//  Created by Chengguangfa on 2019/2/20.
+//  Copyright © 2019年 com.medosport.mo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
