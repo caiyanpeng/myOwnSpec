84e6b0b6865a97c2a9bcdda2edb93bddd97bf816
caiyanpeng <1104990855@qq.com>
2019-02-20 13:12:12 +0800

    init lib Father

---
+++ CAIModularizationTest/CAIModularizationTest/common/Father.h
@@ -0,0 +1,16 @@
+//
+//  Father.h
+//  CAIModularizationTest
+//
+//  Created by Chengguangfa on 2019/2/20.
+//  Copyright © 2019年 com.medosport.mo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Father : NSObject
++(void)sayHello;
+@end
+
+
---
+++ CAIModularizationTest/CAIModularizationTest/common/Father.m
@@ -0,0 +1,16 @@
+//
+//  Father.m
+//  CAIModularizationTest
+//
+//  Created by Chengguangfa on 2019/2/20.
+//  Copyright © 2019年 com.medosport.mo. All rights reserved.
+//
+
+#import "Father.h"
+
+@implementation Father
++(void)sayHello
+{
+        NSLog(@"hello World!!");
+}
+@end
